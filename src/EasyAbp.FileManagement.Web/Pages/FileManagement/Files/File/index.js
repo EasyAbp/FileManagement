3442a404e6b5c5b70769de4f6a088b6088ebd4d6
gdlcf88 <gdlcf88@outlook.com>
2020-08-18 21:35:08 +0800

    Use Math.pow()

--- src/EasyAbp.FileManagement.Web/Pages/FileManagement/Files/File/index.js
+++ src/EasyAbp.FileManagement.Web/Pages/FileManagement/Files/File/index.js
@@ -154,7 +154,7 @@ $(function () {
             ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
             : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
         let u = -1;
-        const r = 10**dp;
+        const r = Math.pow(10, dp) ;
 
         do {
             bytes /= thresh;
