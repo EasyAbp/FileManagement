a715660ede9bef4ac047fa18552d588d7c75a635
Slarkerino Company Desktop <307221214@qq.com>
2021-07-05 15:18:52 +0800

    Added CreateFileEto to enable remote file creation but record via MQ

---
+++ src/EasyAbp.FileManagement.Domain.Shared/EasyAbp/FileManagement/Files/CreateFileEto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EasyAbp.FileManagement.Files
+{
+    [Serializable]
+    public class CreateFileEto
+    {
+        [Required]
+        public string FileContainerName { get; set; }
+
+        [Required]
+        public string FileName { get; set; }
+
+        public string MimeType { get; set; }
+
+        public FileType FileType { get; set; }
+
+        public Guid? ParentId { get; set; }
+
+        public Guid? OwnerUserId { get; set; }
+    }
+}
---
+++ src/EasyAbp.FileManagement.Domain/EasyAbp/FileManagement/Files/CreateFileEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.EventBus.Distributed;
+using Volo.Abp.Uow;
+
+namespace EasyAbp.FileManagement.Files
+{
+    public class CreateFileEventHandler : IDistributedEventHandler<CreateFileEto>, ITransientDependency
+    {
+        private readonly IFileRepository _fileRepository;
+
+        private readonly IFileManager _fileManager;
+
+        public CreateFileEventHandler(IFileRepository fileRepository, IFileManager fileManager)
+        {
+            _fileRepository = fileRepository;
+            _fileManager = fileManager;
+        }
+
+        [UnitOfWork(true)]
+        public virtual async Task HandleEventAsync(CreateFileEto eventData)
+        {
+            var parent = eventData.ParentId.HasValue ? await _fileRepository.FindAsync(eventData.ParentId.Value) : null;
+
+            var file = await _fileManager.CreateAsync(eventData.FileContainerName, eventData.OwnerUserId, eventData.FileName, eventData.MimeType, eventData.FileType, parent, null);
+
+            await _fileRepository.InsertAsync(file, true);
+        }
+    }
+}
