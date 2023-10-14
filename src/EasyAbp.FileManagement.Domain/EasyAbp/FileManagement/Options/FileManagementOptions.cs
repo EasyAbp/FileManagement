8b060a3ff4331dc6883862b14ce16071b972f0d8
gdlcf88 <gdlcf88@outlook.com>
2023-10-14 23:55:29 +0800

    Revised `FileManagementOptionsBase`

--- src/EasyAbp.FileManagement.Domain/EasyAbp/FileManagement/Options/FileManagementOptions.cs
+++ src/EasyAbp.FileManagement.Domain/EasyAbp/FileManagement/Options/FileManagementOptions.cs
@@ -1,7 +1,9 @@
-﻿using EasyAbp.FileManagement.Options.Containers;
+﻿using System;
+using EasyAbp.FileManagement.Options.Containers;
 
 namespace EasyAbp.FileManagement.Options;
 
 public class FileManagementOptions : FileManagementOptionsBase<FileContainerConfiguration>
 {
+    public Type DefaultFileDownloadProviderType { get; set; }
 }
