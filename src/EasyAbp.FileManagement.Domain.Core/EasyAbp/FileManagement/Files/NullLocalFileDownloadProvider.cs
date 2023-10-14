b11d0d4f669c5580ac3118547631cd786c65b63e
gdlcf88 <gdlcf88@outlook.com>
2023-10-14 22:24:38 +0800

    Add a default `NullLocalFileDownloadProvider`

---
+++ src/EasyAbp.FileManagement.Domain.Core/EasyAbp/FileManagement/Files/NullLocalFileDownloadProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+
+namespace EasyAbp.FileManagement.Files;
+
+[Dependency(TryRegister = true)]
+public class NullLocalFileDownloadProvider : ILocalFileDownloadProvider, ISingletonDependency
+{
+    public Task<FileDownloadInfoModel> CreateDownloadInfoAsync(File file)
+    {
+        return Task.FromResult(new FileDownloadInfoModel());
+    }
+
+    public Task CheckTokenAsync(string token, Guid fileId)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task<byte[]> GetDownloadBytesAsync(File file)
+    {
+        return Task.FromResult<byte[]>(null);
+    }
+
+    public Task<Stream> GetDownloadStreamAsync(File file)
+    {
+        return Task.FromResult<Stream>(null);
+    }
+}
