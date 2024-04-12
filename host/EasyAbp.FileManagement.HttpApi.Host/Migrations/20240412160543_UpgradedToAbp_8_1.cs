c44db64655b45d8b7b97ad3c080dbfc380b6e580
gdlcf88 <gdlcf88@outlook.com>
2024-04-13 00:10:56 +0800

    Update the HttpApi.Host project

---
+++ host/EasyAbp.FileManagement.HttpApi.Host/Migrations/20240412160543_UpgradedToAbp_8_1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyAbp.FileManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpgradedToAbp_8_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MimeType",
+                table: "EasyAbpFileManagementFiles",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(32)",
+                oldMaxLength: 32,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MimeType",
+                table: "EasyAbpFileManagementFiles",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128,
+                oldNullable: true);
+        }
+    }
+}
